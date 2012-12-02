87d45c3d43d13387208d42168299af6f3072daca
kanakodavis <matthiasgusenbauer@gmx.net>
2012-12-02 15:50:26 +0100

    Added some raw ideas for Map and Car.

---
+++ Car.java
@@ -0,0 +1,26 @@
+/**
+ * @author Matthias Gusenbauer, Wolfgang Hofer, Alexander Neff
+ */
+
+public class Car implements Runnable {
+
+	public Car(){
+		
+		
+		
+	}
+
+	@Override
+	public void run() {
+		// TODO Auto-generated method stub
+		
+	}
+	
+	public void move() {//nach einer gewissen zeit bewegt sich das auto auf ein neues feld
+		
+		
+		
+	}
+	
+	
+}
---
+++ Map.java
@@ -0,0 +1,20 @@
+/**
+ * @author Matthias Gusenbauer, Wolfgang Hofer, Alexander Neff
+ */
+
+public class Map {
+	
+	//hat liste mit autos
+	public Map() {
+		
+		
+		
+	}
+	
+	public boolean checkHit(int x, int y/*int direction?????*/) {//untersucht ob an den angegebenen korrdinaten bereits ein auto steht und ob es ein hit ist
+		
+		return false;
+		
+	}
+	
+}
