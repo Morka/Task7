4518819cbf539bc92b9b6a9cbafcc29ce7709365
kanakodavis <matthiasgusenbauer@gmx.net>
2012-12-02 15:23:12 +0100

    Added Test.java

---
+++ Test.java
@@ -0,0 +1,15 @@
+/**
+ * @author Matthias Gusenbauer, Wolfgang Hofer, Alexander Neff
+ */
+
+public class Test {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		
+		System.out.println("PEWPEW - ich bin eine testmaschine");
+		
+	}
+}
